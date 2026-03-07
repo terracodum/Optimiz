d42bb1cfb9a41013460bb5b33221aea0fd804961
terracodum <nikitakharlov007@gmail.com>
2026-03-07 12:55:24 +0600

    feat(lab1): add golden ratio method

--- lab1/gold.py
+++ lab1/gold.py
@@ -1,4 +1,25 @@
 ## Метод золотого сечения
 
-def gold():
-    return 1
+import math
+
+
+def goldenRatioMethod(func: function, a: float, b:float, eps: float, iter_count: int, max=False) -> float:
+
+    if (max == True):
+        tau = ((math.sqrt(5) - 1)/2)
+    else:
+        tau = 1-((math.sqrt(5) - 1)/2)
+    l = b - a 
+    x_1 = a + l * tau
+    x_2 = b - l * tau
+    f1 = func(x_1)
+    f2 = func(x_2)
+
+    if (l < eps):
+        midx = (x_1 + x_2)/2
+        return[midx, func(midx), iter_count]
+    if (f1 < f2):
+        return goldenRatioMethod(func, a, x_2, eps, iter_count + 1)
+    
+    if (f1 > f2):
+        return goldenRatioMethod(func, x_1, b, eps, iter_count + 1)
--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,1 +1,5 @@
 from gold import goldenRatioMethod as grm
+from fibon import fibonacciMethod as fib
+import math
+
+print(grm(math.sin, -math.pi, math.pi/2, 0.01, 0))
