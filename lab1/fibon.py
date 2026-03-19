5cc528de2cae1fe9f60fa90c45e9e8191c2ce4fb
Temart00f <temerbalinovartur@mail.ru>
2026-03-19 11:15:24 +0600

    feat(lab1): fibon"

--- lab1/fibon.py
+++ lab1/fibon.py
@@ -1,9 +1,13 @@
 import math
 
 def fib_nums(n):
+    list = [1]
     for i in range(n):
-        ()
+        list.append(list[i] + i)
+    return list
 
 
 def fib(func: callable, a: float, b: float, eps: float, iter_count: float):
-    iter = math.log2()
+    iter = 0
+
+print(fib_nums(3))
